4c01014f504eb4034b69f3fec2518a67e6c03a15
Wilfred Njenga <w.maina.njenga@gmail.com>
2023-10-10 22:58:26 +0300

    Update main.js

--- Registration form/main.js
+++ Registration form/main.js
@@ -1,1 +1,13 @@
+function formSubmitHandler() {
+  // grab inputs
+  const passwordInput = document.getElementById("password");
+  const confirmPasswordInput = document.getElementById("confirmPassword");
 
+  // validation logic
+  if (passwordInput.value !== confirmPasswordInput.value) {
+    confirmPasswordInput.setCustomValidity("The passwords don't match!");
+    confirmPasswordInput.reportValidity();
+    return;
+  }
+  // handle valid inputs ...
+}
